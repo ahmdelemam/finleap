3f70e9b92378516d856edc542a15efbc961aa50c
ahmdelemam <ahmdelemam@gmail.com>
2020-10-31 14:39:16 +0100

    create finleap gem
    bundle gem fineleap

---
+++ lib/fineleap.rb
@@ -0,0 +1,6 @@
+require "fineleap/version"
+
+module Fineleap
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/fineleap_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Fineleap do
+  it "has a version number" do
+    expect(Fineleap::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
